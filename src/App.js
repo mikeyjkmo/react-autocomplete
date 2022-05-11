9cb9763988d1117a014d13582213b51b9de85d7f
Mikey Mo <mikeyjkmo@gmail.com>
2022-05-11 10:02:46 +0100

    Fetch autocompleted Marvel characters with debounce

--- src/App.js
+++ src/App.js
@@ -1,18 +1,68 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-const ITEMS = ["hello", "goodbye", "foobar", "hear"];
+// Hook - I did not write this hook
+function useDebounce(value, delay) {
+  // State and setters for debounced value
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(
+    () => {
+      // Update debounced value after delay
+      const handler = setTimeout(() => {
+        setDebouncedValue(value);
+      }, delay);
+      // Cancel the timeout if value changes (also on delay change or unmount)
+      // This is how we prevent debounced value from updating if value is changed ...
+      // .. within the delay period. Timeout gets cleared and restarted.
+      return () => {
+        clearTimeout(handler);
+      };
+    },
+    [value, delay] // Only re-call effect if value or delay changes
+  );
+  return debouncedValue;
+}
+
+async function fetchMarvelCharacters(nameStartsWith) {
+  if (!nameStartsWith || !nameStartsWith.length) {
+    return {
+      data: {
+        results: [],
+      },
+    };
+  }
+
+  const res = await fetch(
+    "https://gateway.marvel.com:443/v1/public/characters?" +
+      new URLSearchParams({
+        nameStartsWith,
+        apikey: process.env.REACT_APP_MARVEL_API_KEY,
+      })
+  );
+  return res.json();
+}
 
 function AutocompleteDropdown() {
   const [text, setText] = useState("");
   const [inFocus, setInFocus] = useState(false);
+  const [matchingItems, setMatchingItems] = useState([]);
+  const debouncedText = useDebounce(text, 1000);
 
-  function filteredItems() {
-    return ITEMS.filter((item) => item.startsWith(text));
-  }
+  useEffect(() => {
+      fetchMarvelCharacters(debouncedText)
+        .then((res) => res.data.results.map((character) => character.name))
+        .then(setMatchingItems);
+  }, [debouncedText]);
 
   function itemTextWithMatch(item) {
-    const firstMatchingIndex = item.indexOf(text);
+    const lowerCaseItem = item.toLowerCase();
+    const lowerCaseText = text.toLowerCase();
+
+    if (!lowerCaseItem.startsWith(lowerCaseText)) {
+      return item;
+    }
+
+    const firstMatchingIndex = lowerCaseItem.indexOf(lowerCaseText);
     return (
       <>
         <span className="matching-letters">
@@ -23,19 +73,20 @@ function AutocompleteDropdown() {
     );
   }
 
-  function dropDown() {
+  function renderDropDown() {
     if (!inFocus) {
       return null;
     }
 
     return (
       <div className="autocomplete-dropdown">
-        {filteredItems().map((item) => (
+        {matchingItems.map((item) => (
           <div key={item}>
             <button
               onMouseDown={(e) => e.preventDefault()}
               onClick={() => {
                 setText(item);
+                setInFocus(false);
               }}
             >
               {itemTextWithMatch(item)}
@@ -56,7 +107,7 @@ function AutocompleteDropdown() {
         onFocus={() => setInFocus(true)}
         onBlur={() => setInFocus(false)}
       />
-      {dropDown()}
+      {renderDropDown()}
     </div>
   );
 }
